050c7014ce69f0ecfe384131af23dd003ef7ba15
ebiliak <ilforoughi@s.sfusd.edu>
2025-01-31 18:09:58 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         PalindromeChecker checker = new PalindromeChecker();
-        checker.tester();  // Call tester method
+        checker.tester();  
     }
 }
